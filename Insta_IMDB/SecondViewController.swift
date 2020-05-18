a90f85de8b3323e0539a79a09d140d458d1a1516
Asser Ayman <asser.ayman@gmail.com>
2020-05-18 20:19:50 +0200

    Initial Commit

---
+++ Insta_IMDB/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Insta_IMDB
+//
+//  Created by Asser on 5/18/20.
+//  Copyright © 2020 Asser. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
